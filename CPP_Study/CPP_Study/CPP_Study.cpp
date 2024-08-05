28af650f0b120f9297885e576afb7b0019a72535
wjstjrah2000 <wjstjrah2000@gmail.com>
2024-08-05 22:01:44 +0900

    0805Home

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -15,26 +15,76 @@
 #include <iomanip>
 #include <string>
 #include <cmath>
+#include <list>
 using namespace std;
 
-// 디버깅
+// list
+// vector와 유사하게 배열의 형태임
+// 하지만 vector : 동적 할당
+// list : 노드
 
-void Test2()
-{
-    int b = 0;
-}
+// vector : 동적 배열, 메모리 증설(더큰메모리새로할당,복사, 이전데이터삭제), 데이터연속배열...
 
-void Test1()
+// Node[    data(4byte) next(4byte:32bit / 8byte:64bit) <- 포인터니까     ]
+class Node
 {
-    int a = 0;
-    Test2();
-}
+public:
+    Node* _next; // 다음에 갈 노드의 주소를 담은 '포인터'
+    Node* _prev; // 이게 있으면 이중(양방향) 리스트가 되는겨
+    int _data;
+};
+
+
+
 
 int main()
 {
-    int number = 10;
+    // list (연결 리스트)
+    // - list의 동작 원리
+    // - 중간 삽입/삭제
+    // - 처음/끝 삽입/삭제
+    // - 임의 접근
 
-    Test1();
+    // list :( 단일 / 이중 / 원형 ) '연결' 리스트
 
+    // 단일 : 노드[1]      ->   노드[2]   ->         노드[3]  ->   노드[4]  ->  노드[5]
+    // 이중 : 노드[1]      <->  노드[2]   <->        노드[3]  <->  노드[4]  <-> 노드[5]
+    // 원형 : 노드[1]      <->  노드[2]   <->        노드[3]  <->  노드[4]  <-> 노드[5] <-> 노드[1]
+
+    list<int> li;
+
+    for (int i = 0; i < 100; ++i)
+    {
+        li.push_back(i);
+    }
+
+    // li.push_front(10);  오 뭐야 이걸 지원하네? -> vector와는 다른 동작 원리
+    int size = li.size();
+    // li.capacity; 없음, 이건 동작 할당이 아니라 이런 개념이 없음
+    int firstValue = li.front();
+    int lastValue = li.back();
+    // li[3] = 10; 없음, 임의 접근을 허용하지 않음
+
+    list<int>::iterator itBegin = li.begin();
+    list<int>::iterator itEnd = li.end();
+
+    for (list<int>::iterator it = li.begin(); it != li.end(); ++it)
+    {
+        cout << *it << endl;
+    }
+
+    li.insert(itBegin, 100);
+
+    li.erase(li.begin()/*, li.end()*/); // 보통 범위 삭제에 쓰이지 
+
+    li.pop_front(); 
+
+    li.remove(10); // 특정 값에 해당하는 데이터들 삭제
+
+
+
+    
+
+    
     return 0;
 }

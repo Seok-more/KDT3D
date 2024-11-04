98cf08d859158f14faabdcb187d788657bf5bd6a
DESKTOP-COA1C72\AR student <test@gmail.com>
2024-11-04 17:51:53 +0900

    Revert "0912 away"
    This reverts commit 85f18bfaad7a4f26995cc7d6b502f3fc0fc389e5.

--- CPP_Study3/CPP_Study3/CPP_Study3/CPP_Study3.cpp
+++ CPP_Study3/CPP_Study3/CPP_Study3/CPP_Study3.cpp
@@ -8,210 +8,136 @@
 #include <list>
 #include <stack>
 #include <queue>
+#include "RedBlackTree/RBTree.h"
 #include <thread>
 
 using namespace std;
 
-// 최소 스패닝 트리(Minimum Spanning Tree)
-// - 그래프/트리 응용
+// 해시테이블
 
-// Disjoint Set(상호 배타적 접합)
-// -> 유니온-파인드(Union-Find) 
+// map vs hash_map(unordered_map)
 
+// map : Red-Black Tree
+// - 추가/탐색/삭제 : O(logN)
 
-// ex
-// LineageBattleground
-// 1인팀 1000명(팀 id : 0~999)
-// 동맹 (1번팀 + 2번팀 = 1번팀)
+// hash_map(unordered_map) 
+// - 추가/탐색/삭제 : O(1)
+// 충돌만 자주 안나면 훨씬 빠름
 
+// 메모리를 내주고 속도를 취한다.
 
-void LineageBattleground()
+// UserID 1~999
+// [1][2][3][4][5]...[999]
+
+// 테이블 : O(1)
+// - 키를 알면 데이터를 찾을 수 있다.
+void TestTable()
 {
     struct User
     {
-        int teamID;
+    public:
+        int userID = 0;
+        string userName;
     };
 
-    // TODO : UserManager
     vector<User> users;
-    for (int i = 0; i < 1000; ++i)
-    {
-        users.push_back(User{ i });
-    }
+    users.resize(1000);
 
-    // 팀 동맹
-    //  users[1] <-> users[5]
-    users[5].teamID = users[1].teamID; // [1]번팀으로 동맹함
+    // 777번 유저 세팅
+    users[777] = User{ 777, "asd" };
 
-    // [0][1][2][3][4]...[999]
-    // [1][1][1]...[2][2]...[999]
-    // teamID = 1 팀과, teamID = 2 팀을 동맹시키려면 
+    // 777번 유저의 이름은 뭔가요?
+    string username777 = users[777].userName;
 
-    for (User& user : users)
-    {
-        if (user.teamID == 1)
-        {
-            user.teamID = 2;
-        }
-    }
-    // -> 찾기 연산 O(1), 합치기 연산 O(N)
+    cout << username777 << endl;
 }
-//---------------------------------------------------------------
 
-// 트리 구조를 이용한 상호 배타적 집합의 표현
 
-// 1) [0][1][2][3][4][5]
-struct Node
+
+// 해시 : O(1)
+void TestHash()
 {
-    Node* leader; // 처음엔 자기가 자기팀의 리더
-};
-//---------------------------------------------------------------
-// 2)    [1]    [3]
-//       [2]   [4][5]
-//                [0]
-// - 상위노드(부모)를 리더로 가리킴
+    struct User
+    {
+    public:
+        int userID = 0;
+        string userName;
+    };
 
-class NaiveDisjointSet
+    vector<User> users;
+    users.resize(1000);
+
+    const int userID = 123456789;
+    int key = (userID % 1000); // hash < 고유번호
+
+    // 123456789번 유저 세팅
+    users[key] = User{ userID, "asd" };
+
+    // 123456789번 유저의 이름은 뭔가요?
+    User& user = users[key];
+
+    if (user.userID = userID)
+    {
+        string username123456789 = user.userName;
+        cout << username123456789 << endl;
+    }
+}
+
+
+// 충돌 문제 (key값이 겹침)
+    // - ex : 123456789 와 789의 키값이 겹침
+    // - 충돌이 발생한 자리를 대신해서 다른 빈자리를 찾는다
+    // -- 선형 조사법 (linear probing)
+    // --- hash(key)+1 -> hash(key)+2
+    // -- 이차 조사법 (quadratic probing)
+    // --- hash(key)+1^2 -> hash(key)+2^2
+    // ...etc
+
+
+
+// 해시테이블체이닝 : O(1)
+void TestHashTableChaining()
 {
-public:
-    NaiveDisjointSet(int n) : _parent(n)
+    struct User
     {
-        for (int i = 0; i < n; ++i)
-        {
-            _parent[i] = i;
-        }
-    }
-public:
-    // 최상위노드를 찾는 함수 (4의 부모는 3, 0의 부모는 5이지만 최상위 노드는 3으로 동일)
-    int Find(int u)
+    public:
+        int userID = 0;
+        string userName;
+    };
+
+    //    [ ]   
+    //    [ ]      [ ]
+    // [ ][ ][ ][ ][ ][ ][ ][ ]
+    vector<vector<User>> users;
+    users.resize(1000);
+
+    const int userID = 123456789;
+    int key = (userID % 1000); // hash < 고유번호
+
+    // 123456789번 유저 세팅
+    users[key].push_back(User{ userID, "asd" });
+    users[789].push_back(User{ 789, "asd" });
+
+    // 123456789번 유저 이름은?
+    vector<User>& bucket = users[key];
+
+    for (User& user : bucket)
     {
-        if (u == _parent[u])
+        if (user.userID == userID)
         {
-            return u;
-        }
-
-        return Find(_parent[u]);
-    }
-//---------------------------------------------------------------
-// 3)             [3]
-//             [4][5][1]
-//                [0][2]
-
-// 시간 복잡도 : 트리의 높이가 비례한 시간이 걸림
-
-    // u와 v를 합친다 (일단 u가 v 밑으로 )
-    // 트리가 한쪽으로 기우는 문제를 해결
-    // -> [높이가 낮은 트리를] [높이가 높은 트리] 밑으로 (Union-By-Rank)
-    void Merge(int u, int v)
-    {
-        u = Find(u);
-        v = Find(v);
-
-        if (u == v)
-        {
-            return;
-        }
-        _parent[u] = v;
-    }
-
-private:
-    vector<int> _parent;
-
-};
-
-//---------------------------------------------------------------
-
-// O(1)
-class DisjointSet
-{
-public:
-    DisjointSet(int n) : _parent(n), _rank(n, 1)
-    {
-        for (int i = 0; i < n; ++i)
-        {
-            _parent[i] = i;
+            string Name = user.userName;
+            cout << Name << endl;
         }
     }
 
-
- //       [1]      [3]
- //       [2]   [4][5][0]
- //                
-public:
-    // 최상위노드를 찾는 함수를 최적화
-    int Find(int u)
-    {
-        if (u == _parent[u])
-        {
-            return u;
-        }
-
-        _parent[u] = Find(_parent[u]);
-        return _parent[u];
-    }
-
-
-    // 시간 복잡도 : 트리의 높이가 비례한 시간이 걸림
-
-        // u와 v를 합친다 (일단 u가 v 밑으로 )
-    void Merge(int u, int v)
-    {
-        u = Find(u);
-        v = Find(v);
-
-        if (u == v)
-        {
-            return;
-        }
-
-        if (_rank[u] > _rank[v])
-        {
-            // rank[u] <= rank[v] 보장됨
-            swap(u, v);
-        }
-
-        _parent[u] = v;
-
-
-// 이런 경우 처리
-//       [1]    [3]
-//       [2]   [4][5]
-//       [6]     [0]
-// ->
-//              [3]
-//           [4][5][1]
-//              [0][2]
-//                 [6]
-
-        if (_rank[u] == _rank[v])
-        {
-            ++_rank[v];
-        }
-
-    }
-
-private:
-    vector<int> _parent;
-    vector<int> _rank;
-};
-
+}
 
 
 int main()
 {
-    DisjointSet teams(1000);
-
-    teams.Merge(10, 1);
-    int teamID = teams.Find(1);
-    int teamID2 = teams.Find(10);
-
-    teams.Merge(3, 2);
-    int teamID3 = teams.Find(3);
-    int teamID4 = teams.Find(2);
-
-    teams.Merge(1, 3);
-    int teamID5 = teams.Find(1);
-    int teamID6 = teams.Find(3);
+    //TestTable();
+    //TestHash();
+    TestHashTableChaining();
 
     return 0;
 }
